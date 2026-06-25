3813b84185b9e796b0c6abfd3494b3dd45af88f0
David Cardenas Lorca <cine.dcl@gmail.com>
2026-06-24 21:02:57 -0400

    feat(cms-domain): add form models with translation support

---
+++ app/Models/FormFieldTranslationModel.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class FormFieldTranslationModel extends Model
+{
+    protected $table      = 'cms_form_field_translations';
+    protected $primaryKey = 'id';
+    protected $returnType = 'array';
+    protected $useTimestamps = false;
+
+    /** @var list<string> */
+    protected $allowedFields = [
+        'form_field_id',
+        'language_id',
+        'label',
+        'placeholder',
+        'help_text',
+        'error_required',
+        'error_invalid',
+    ];
+}
